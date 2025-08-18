1e887d5fd91f513000a0be0bc24e8fe026993a05
too_long_name <mail@too-long-name.io>
2025-08-19 00:20:25 +0800

    :sparkles: 好久没更了，更一下

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
+
 import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://xn--jp8ha.ws",
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap()],
   vite: {
     build: {
       rollupOptions: {
